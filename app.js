34b90f76ad723ad6300a9217a07e33322245e952
Anshuman Tiwari <anshumantiwari.cse@gmail.com>
2024-12-18 22:27:21 +0530

    updated apppppp

--- app.js
+++ app.js
@@ -2,29 +2,26 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import routes from './routes/index.js';
-import path from 'path';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import cloudinary from 'cloudinary';
 import morgan from 'morgan';
 import moment from 'moment-timezone';
+
 const app = express();
 
-dotenv.config({
-  path: './.env'
-});
+dotenv.config({ path: './.env' });
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 const __filename = fileURLToPath(import.meta.url);
 
-
-  // Custom format string with the IST timestamp token
-  const morganFormat = '":method :url HTTP/:http-version" :status :res[content-length] ":referrer"';
-  
+// Custom morgan format for logging
+const morganFormat = '":method :url HTTP/:http-version" :status :res[content-length] ":referrer"';
 
 // Middleware to log HTTP requests using morgan
 app.use(morgan(morganFormat, {
@@ -35,43 +32,19 @@ app.use(morgan(morganFormat, {
   }
 }));
 
-
 // Custom token to log timestamp in IST
 morgan.token('istDate', (req, res) => {
   return moment().tz('Asia/Kolkata').format('DD/MMM/YYYY:HH:mm:ss ZZ');
 });
 
-
-// app.use(cors(
-//   {
-//     origin: "https://www.pkphotography.io",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   }
-// ));
-
-const allowedOrigins = [
-  '*'
-];
-
+// CORS configuration to allow all origins
 app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin, like mobile apps or curl requests
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true // Allow sending cookies or other credentials
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true // Enable credentials if necessary
 }));
 
-
-app.use(express.json({
-  limit: '50mb'
-}));
-
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1', routes);
 
@@ -79,10 +52,11 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
+
 app.get('/', (req, res) => {
-  res.send('Server is runnning...');
-})
+  res.send('Server is running...');
+});
 
 connectDB();
 
-export default app;
+export default app;
