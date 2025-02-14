bb4f75a37f2b32bd46908f83f4748a85fc5b4076
anshuman <anshuman.tiwari@kindasolutions.com>
2025-02-14 18:26:43 +0530

    updated

---
+++ services/passport.js
@@ -0,0 +1,53 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../models/user.js";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL, 
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ email: profile.emails[0].value });
+
+        if (!user) {
+          user = new User({
+            fullName: profile.displayName, // Store Google name
+            email: profile.emails[0].value,
+            profileImage: profile.photos[0]?.value || "",
+            isverify: true,
+            isActive: true,
+          });
+
+          await user.save();
+        } else if (!user.fullName) {
+          // Update existing user if fullName is missing
+          user.fullName = profile.displayName;
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+export default passport;
