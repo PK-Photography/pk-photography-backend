411302ae6c4efd244c590d924c9bac69298e16ef
anshuman <anshuman.tiwari@kindasolutions.com>
2025-02-17 18:04:40 +0530

    updated-clienrt

--- models/cardModel.js
+++ models/cardModel.js
@@ -1,3 +1,39 @@
+// import mongoose from 'mongoose';
+
+// const cardSchema = mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       default: null,
+//     },
+//     date: {
+//       type: Date,
+//       default: null,
+//     },
+//     imageUrl: {
+//       type: String,
+//       required: true,
+//     },
+//     canDownload:{type: Boolean},
+//     category: [
+//       {
+//         name: { type: String, required: true },
+//         images: { type: String }
+//       }
+//     ]
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+
+// const Card = mongoose.model('Card', cardSchema);
+
+// export default Card;
+
+
+
 import mongoose from 'mongoose';
 
 const cardSchema = mongoose.Schema(
@@ -19,13 +55,20 @@ const cardSchema = mongoose.Schema(
         name: { type: String, required: true },
         images: { type: String }
       }
-    ]
+    ],
+    canDownload: {
+      type: Boolean,
+      default: true,
+    },
+    canView: {
+      type: Boolean,
+      default: true,
+    }
   },
   {
     timestamps: true,
   }
 );
-
 
 const Card = mongoose.model('Card', cardSchema);
 
