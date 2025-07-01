653768c162dfdca750434f57c8504e80ae1b7bae
Toshita Pandey <toshitapandey@gmail.com>
2025-07-01 21:05:21 +0530

    add blog routes

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,15 +1,32 @@
 import Blog from "../models/blogModel.js";
+import cloudinary from "cloudinary";
 
 // CREATE
 export const createBlog = async (req, res) => {
   try {
-    const { title, subtitle, imageUrl, author, content, date } = req.body;
+    const { title, subtitle, author, content, date, image } = req.body;
 
     if (!title || !content) {
       return res.status(400).json({ message: "Title and content are required" });
     }
 
-    const blog = new Blog({ title, subtitle, imageUrl, author, content, date });
+    let imageUrl = "";
+    if (image) {
+      const uploadResponse = await cloudinary.v2.uploader.upload(image, {
+        folder: "blogs",
+      });
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const blog = new Blog({
+      title,
+      subtitle,
+      author,
+      content,
+      date,
+      image: imageUrl,
+    });
+
     await blog.save();
     res.status(201).json({ message: "Blog created", data: blog });
   } catch (error) {
@@ -41,22 +58,29 @@ export const getBlogById = async (req, res) => {
 // UPDATE
 export const updateBlog = async (req, res) => {
   try {
-    const { title, subtitle, imageUrl, author, content, date } = req.body;
+    const { title, subtitle, author, content, date, image } = req.body;
 
-    const updatedBlog = await Blog.findByIdAndUpdate(
-      req.params.id,
-      { title, subtitle, imageUrl, author, content, date },
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) return res.status(404).json({ message: "Blog not found" });
 
-    if (!updatedBlog) {
-      return res.status(404).json({ message: "Blog not found" });
+    let imageUrl = blog.image;
+
+    if (image && image !== blog.image) {
+      const uploadResponse = await cloudinary.v2.uploader.upload(image, {
+        folder: "blogs",
+      });
+      imageUrl = uploadResponse.secure_url;
     }
 
-    res.status(200).json({ message: "Blog updated", data: updatedBlog });
+    blog.title = title || blog.title;
+    blog.subtitle = subtitle || blog.subtitle;
+    blog.author = author || blog.author;
+    blog.content = content || blog.content;
+    blog.date = date || blog.date;
+    blog.image = imageUrl;
+
+    await blog.save();
+    res.status(200).json({ message: "Blog updated", data: blog });
   } catch (error) {
     res.status(500).json({ message: "Error updating blog", error: error.message });
   }
--- routes/v1/blogRoutes.js
+++ routes/v1/blogRoutes.js
@@ -9,10 +9,10 @@ import {
 
 const router = express.Router();
 
-router.post("/blogs", createBlog);
+router.post("/blogs", createBlog);           // no multer needed
 router.get("/blogs", getBlogs);
 router.get("/blogs/:id", getBlogById);
-router.put("/blogs/:id", updateBlog);
+router.put("/blogs/:id", updateBlog);        // no multer needed
 router.delete("/blogs/:id", deleteBlog);
 
 export default router;
