e4152cb44847e290c83f986af7ca06c40cf3f150
Puneet Jain <jainpuneet711@gmail.com>
2025-09-18 12:14:34 +0530

    favourite image apis added

--- routes/v1/userRoutes.js
+++ routes/v1/userRoutes.js
@@ -1,10 +1,27 @@
 import express from "express";
-import { getAllUsers, getProfile, getUserProfile, googleAuth, googleAuthController, login, updateUserRole, UserForgotPassword, UserResetPassword, UserSignUp, UserVerifyEmailOTP } from '../../controllers/userController.js'
-import { checkAuthenticate, verifyUser } from "../../middleware/adminAuthenticate.js";
+import {
+  getAllUsers,
+  getProfile,
+  getUserProfile,
+  googleAuth,
+  googleAuthController,
+  login,
+  updateUserRole,
+  UserForgotPassword,
+  UserResetPassword,
+  UserSignUp,
+  UserVerifyEmailOTP,
+  addFavourite,
+  removeFavourite,
+  getFavourites,
+} from "../../controllers/userController.js";
+import {
+  checkAuthenticate,
+  verifyUser,
+} from "../../middleware/adminAuthenticate.js";
 import { verifyUserToken } from "../../services/authServices.js";
 
 const router = express.Router();
-
 
 const testMiddleware = (req, res, next) => {
   console.log(`[Test Middleware Triggered]: ${req.method} ${req.originalUrl}`);
@@ -13,24 +30,22 @@ const testMiddleware = (req, res, next) => {
 
 //  ===================|| Auth Routes ||======================
 // router.post("/booking/email", sendBookingEmail);
-router.post('/user/signup', UserSignUp);
-router.post('/user/verify_otp', UserVerifyEmailOTP);
-router.post('/user/login', login);
-router.post('/user/reset-password', UserResetPassword);
-router.post('/user/forgot-password', UserForgotPassword);
-router.get('/user/profile', verifyUser, getUserProfile);
-router.get('/user/all', getAllUsers);
-router.put('/user/update-role', updateUserRole);
-
+router.post("/user/signup", UserSignUp);
+router.post("/user/verify_otp", UserVerifyEmailOTP);
+router.post("/user/login", login);
+router.post("/user/reset-password", UserResetPassword);
+router.post("/user/forgot-password", UserForgotPassword);
+router.get("/user/profile", verifyUser, getUserProfile);
+router.get("/user/all", getAllUsers);
+router.put("/user/update-role", updateUserRole);
+router.put("/user/addFavourite", addFavourite);
+router.put("/user/removeFavourite", removeFavourite);
+router.get("/user/getFavourites/:userId", getFavourites);
 
 router.get("/google", googleAuth);
-
 
 router.post("/google-login", googleAuthController);
 // Get Profile
 router.get("/profile", getProfile);
-
-
-
 
 export default router;
